fc96301cb9657bc6f548208211ab5f6e8f2a3c5c
ZBurnell <136769106+ZBurnell@users.noreply.github.com>
2023-10-29 21:01:59 -0400

    Wrote code for Shapes and test. Inquirer won't run

---
+++ index.js
@@ -0,0 +1,69 @@
+//Packages used to create a Logo in Node
+const inquirer = require('inquirer');
+const fs = require('fs')
+const { Triangle, Square, Circle } = require("./lib/shapes");
+
+
+function promptUser() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Please enter 3 Characters to start your new logo",
+        name: "Text",
+      },
+      {
+        type: "input",
+        message: "Please Enter a color or hex code for the text",
+        name: "TextColor",
+      },
+      {
+        type: "list",
+        message: "Please Choose a shape for your new logo",
+        choices: ["Circle", "Square", "Triangle"],
+        name: "shape",
+      },
+      {
+        type: "input",
+        message: "Please Enter a color or hex code for the background",
+        name: "Background",
+      },
+    ])
+    .then((answers) => {
+      if (answers.text.length > 3) {
+        console.log("Must enter a value of no more than 3 characters");
+        promptUser();
+      } else {
+        writeToFile("logo.svg", answers);
+      }
+    });
+}
+promptUser();
+
+// Function to write SVG file using user answers
+function writeToFile(fileName, answers) {
+    let svgString = "";
+    svgString = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+    svgString += "<g>";
+    svgString += `${answers.shape}`;
+  
+    let shapeChoice;
+    if (answers.shape === "circle") {
+      shapeChoice = new Circle();
+      svgString += `<circle cx="150" cy="115" r="80" fill="${answers.Background}"/>`;
+    } else if (answers.shape === "Square") {
+      shapeChoice = new Square();
+      svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.Background}"/>`;
+    } else {
+      shapeChoice = new Triangle();
+      svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.Background}"/>`;
+    }
+  
+    svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.TextColor}">${answers.Text}</text>`;
+    svgString += "</g>";
+    svgString += "</svg>";
+  
+    fs.writeFile(fileName, svgString, (err) => {
+      err ? console.log(err) : console.log("logo.svg has been created successfully");
+    });
+  }
